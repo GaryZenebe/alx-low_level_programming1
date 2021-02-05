d0b8e0801e1eceaac160196013dab50143d30787
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-05 20:44:14 +0000

    task 10 done

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+
