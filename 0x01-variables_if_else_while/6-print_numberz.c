fe56268171d922685548828d3dff06b5cfa800c8
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-05 20:05:40 +0000

    issues fixed

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
 */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{
-		putchar('0' + n);
-	}
-	putchar('\n')
+	putchar('0' + n);
+	putchar('\n');
 	return (0);
 }
