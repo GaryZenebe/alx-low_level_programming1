b440259a4e40234383569d7b2f9a61fbb7773554
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-09 08:39:18 +0000

    task 0 completed

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - this is holberton main function
+ *
+*/
+int main(void)
+{
+	char text[] = "Holberton";
+
+	for (char i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar("\n");
+	return (0);
+}
