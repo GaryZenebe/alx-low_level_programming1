6addf2dcee55a5b5b7143d297888254fdaf8d218
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-12 20:53:02 +0000

    task 6 done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_line - prints line
+ * @n: the char
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i = n;
+
+	for (i = n; i > 0; i--)
+		_putchar('_');
+	_putchar('\n');
+}
