d6c6e5bdd5177eb7b3a10565c9797e7f7b50ebe0
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-12 20:58:20 +0000

    task done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_fizzbuzz - prints num
+ * Return: nothing
+ */
+void print_fizzbuzz(void)
+{
+	int i = 1;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 && i % 5)
+		{
+		printf("FizzBuzz");
+		}
+		else if (i % 3)
+		{
+		printf("Fizz");
+		continue;
+		}
+		else if (i % 5)
+		{
+		printf("Buzz");
+		}
+		else
+		{
+		printf("%d", i);
+		}
+	}
+}
