a9270112ecc9c7a985481f2c6bca0e70112abac7
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-16 09:52:50 +0000

    task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - count the length of a string
+ * @s: char pointer
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
