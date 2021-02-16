6ec3ca0f144fb25e27f71c80257e913510c15c57
GaryZenebe <Gedion.Zenebe@gmail.com>
2021-02-16 13:09:57 +0000

    task 4 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - print string in reverse
+ * @s: character literal
+ * Return: nothing
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,4 +1,6 @@
-int _puthcar(char);
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
